2b46d9b95bb00f62e01d26b805c9081b17550907
KhiemGOM <khiem06072007@gmail.com>
2023-02-28 22:54:22 +0700

    Merge AbsEncoder to Turret to avoid cluttering and use getDistance() instead

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -7,15 +7,33 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.CAN_ID.*;
 
 public class Turret extends SubsystemBase {
   public WPI_TalonSRX turret = new WPI_TalonSRX(TURRET);
+  private AnalogEncoder absEncoder;
   /** Creates a new Turret. */
-  public Turret() {
+  public Turret(int channel) {
+    absEncoder = new AnalogEncoder(channel);
+    absEncoder.setDistancePerRotation(2*Math.PI); //Such that a revolution = 2pi radian
     turret.setNeutralMode(NeutralMode.Brake);
+  }
+
+
+  /** Reset absolute encoder */
+  public void reset() {
+    absEncoder.reset();
+  }
+
+  /**
+   * Get absolute encoder value
+   * @return current angle in radian
+  */
+  public double get() {
+    return absEncoder.getDistance();
   }
 
   public void rotate(double x) {
@@ -25,5 +43,6 @@ public class Turret extends SubsystemBase {
   public void periodic() {
     SmartDashboard.putNumber("Turret velocity", turret.get());
     SmartDashboard.putNumber("Talon Turret Temp", turret.getTemperature());
+    SmartDashboard.putNumber("Absolute encoder value (radian)", get());
   }
 }
